e9bdec3dddcee058a64709b8d13f1cdc93df643c
vkduong <47140552+cutcutaye@users.noreply.github.com>
2022-12-15 15:45:08 +0900

    Update run_inference.sh

--- src/edge_tss/tfe/bin/run_inference.sh
+++ src/edge_tss/tfe/bin/run_inference.sh
@@ -5,3 +5,8 @@ python main.py  \
    --dataset aicity2021_final  \
    --config cam_1.yaml  \
    --write_video True
+
+# Run all cameras
+#python main_all.py  \
+#   --dataset aicity2021_final \
+#   --write_video True
